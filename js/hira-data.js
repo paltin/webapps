363d70c864e1e28c50ab1e7cbe90bb27326327eb
paltinyan <paltin.yan@gmail.com>
2025-10-07 20:19:49 +0300

    Step 1-2: add standalone Hiragana data module and placeholder filter CSS (not wired)

---
+++ js/hira-data.js
@@ -0,0 +1,4 @@
+﻿// Hiragana data module (no integration yet)
+export const HIRAGANA_DATA = [ { symbol: 'あ', romaji: 'a', group: 'base' } ];
+export const BASE_SYMBOLS = ['あ'];
+export const GRID_LAYOUT = [['あ', null, null, null, null]];
