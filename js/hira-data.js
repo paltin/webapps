2d99b4924665776ffff257602c205b7e48f9ab2b
paltinyan <paltin.yan@gmail.com>
2025-10-07 20:43:29 +0300

    Revert "Step 1-2: add standalone Hiragana data module and placeholder filter CSS (not wired)"
    This reverts commit 363d70c864e1e28c50ab1e7cbe90bb27326327eb.

--- js/hira-data.js
+++
@@ -1,4 +0,0 @@
-﻿// Hiragana data module (no integration yet)
-export const HIRAGANA_DATA = [ { symbol: 'あ', romaji: 'a', group: 'base' } ];
-export const BASE_SYMBOLS = ['あ'];
-export const GRID_LAYOUT = [['あ', null, null, null, null]];
